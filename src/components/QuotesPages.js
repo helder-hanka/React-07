b4e69db87c0850efb9aa1525497ce053dcd3cc1a
helder-hanka <hfernandes238@gmail.com>
2020-04-15 22:14:18 +0200

    Creat components for quotes simpsons

---
+++ src/components/QuotesPages.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import axios from 'axios'
+
+import './QuotesPages.css'
+
+class QuotesPages extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            simpsons: []
+        }
+    }
+    // getSimpsons = () => {
+    //     axios.get('https://simpsons-quotes-api.herokuapp.com/quotes?count=1')
+    //     //.then(Response => Response.data)
+    //     //.then(data => {this.setState({simpsons: data.quote[0]})})
+    //     .then(res => console.log(res) || this.setState({simpsons: res.data[0]}))
+    //     //.then(res => this.setState({simpsons: res.data}))
+    // }
+
+    getCharacters = () => {
+        axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+        // .then(res => console.log(res) || this.setState({simpsons: res.data}))
+        .then(res => this.setState({simpsons: res.data}))
+    }
+
+    componentDidMount (d) {
+        this.getCharacters()
+    }
+    render() {
+        //console.log(this)
+        const {simpsons} = this.state
+        return (
+            <div className="QuotesPages">
+                {simpsons.map((simpson) => console.log(simpson) || (
+                    <div>
+                        <p key={simpson.character}>{simpson.character}</p>
+                        <p key={simpson.quote}>{simpson.quote}</p>
+                        <img src={simpson.image} alt={simpson.character} />
+                    </div>
+                    
+                    )
+                )}
+                <div>
+                    <button type="button" onClick={this.getCharacters}>get Simpsons</button>
+                </div>
+            </div>
+        )
+
+    }
+}
+
+export default QuotesPages
